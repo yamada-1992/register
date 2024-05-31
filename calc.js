8daeb3f53adf167c4c65679a3fe6f81b24914a38
yamada1992 <85730996+yamada-1992@users.noreply.github.com>
2024-06-01 01:02:39 +0900

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,21 @@
+function count(id, num){
+    if (id != "" && num != 0) {
+        value = document.getElementById(id)
+        value.innerHTML = parseInt(value.textContent)+num
+    }
+
+    cost = (parseInt(document.getElementById("pudding").textContent) * 300) + (parseInt(document.getElementById("cider_can").textContent) * 300) + (document.getElementById("take_out").checked * 100)
+    document.getElementById("cost_display"). textContent = "¥" + cost
+}
+
+function send(){
+    pudding = document.getElementById("pudding").textContent
+    takeout = document.getElementById("take_out").checked
+    cidercan = document.getElementById("cider_can").textContent
+    
+    fetch("https://script.google.com/macros/s/AKfycbwi_jcWyZl6tGOe-sigfRDPIwde_jatPjlbKjSW_Iqf7i3nV1vHCe9TWiu-EnbEsWEE/exec?pudding="+pudding+"&takeout="+takeout+"&cidercan="+cidercan)
+    document.getElementById("cost_display").textContent = "¥0"
+    document.getElementById("pudding").textContent = "0"
+    document.getElementById("take_out").checked = false
+    document.getElementById("cider_can").textContent = "0"
+}
